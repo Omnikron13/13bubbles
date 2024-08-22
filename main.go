2f4a6ccbe98b1b2db08dd1c5b97904d9ebd2fb96
Joey Sabey <joey.sabey@gmx.com>
2024-08-22 16:10:37 +0100

    feature: add RenderPrefix() & RenderSuffix() lambdas to main.go test/demo

--- main.go
+++ main.go
@@ -14,6 +14,60 @@ func main() {
    l.Selectable = true
    l.Focussed = true
 
+   l.RenderPrefix = func(s string) string {
+      switch s {
+         case "one":
+            return "㊀"
+         case "two":
+            return "㊁"
+         case "three":
+            return "㊂"
+         case "four":
+            return "㊃"
+         case "five":
+            return "㊄"
+         case "six":
+            return "㊅"
+         case "seven":
+            return "㊆"
+         case "eight":
+            return "㊇"
+         case "nine":
+            return "㊈"
+         case "ten":
+            return "㊉"
+         default:
+            return ""
+      }
+   }
+
+   l.RenderSuffix = func(s string) string {
+      switch s {
+         case "one":
+            return " Ⅰ"
+         case "two":
+            return " Ⅱ"
+         case "three":
+            return " Ⅲ"
+         case "four":
+            return " Ⅳ"
+         case "five":
+            return " Ⅴ"
+         case "six":
+            return " Ⅵ"
+         case "seven":
+            return " Ⅶ"
+         case "eight":
+            return " Ⅷ"
+         case "nine":
+            return " Ⅸ"
+         case "ten":
+            return " Ⅹ"
+         default:
+            return ""
+      }
+   }
+
    p := bt.NewProgram(&l, bt.WithAltScreen())
 
    if m, err := p.Run(); err != nil { bt.ErrProgramKilled.Error() } else {
