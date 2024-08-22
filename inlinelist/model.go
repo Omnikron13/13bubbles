cb4c08a62945eb10a63ac2b2d37e8f9a22fb4ffc
Joey Sabey <joey.sabey@gmx.com>
2024-08-22 11:00:14 +0100

    refactor: move style structs etc. into styles.go

--- inlinelist/model.go
+++ inlinelist/model.go
@@ -6,81 +6,8 @@ import (
    "unicode/utf8"
 
    bt "github.com/charmbracelet/bubbletea"
-   lg "github.com/charmbracelet/lipgloss"
    "github.com/charmbracelet/bubbles/key"
 )
-
-
-// ItemStyle is a type grouping lipgloss styles for a list item and its prefix and suffix, to facilitate
-// applying a variety of different styles for different states of the list item (e.g. selected, focussed, etc.)
-type ItemStyle struct {
-   Main lg.Style
-   Prefix lg.Style
-   Suffix lg.Style
-}
-
-// ItemStyleStates is a type grouping ItemStyle structs for different states of a list item
-type ItemStyleStates struct {
-   Normal ItemStyle
-   Selected ItemStyle
-}
-
-// Styles is a type grouping lipgloss styles for a list and its items, to facilitate applying different styles
-// to different states of the list (e.g. focussed, etc.)
-type Styles struct {
-   List lg.Style
-   Item ItemStyleStates
-   Seperator lg.Style
-}
-
-// DefaultStyle returns a new Styles struct with the given styles.
-func DefaultStyles() (unfocussed Styles, focussed Styles) {
-   unfocussedItemNormal := ItemStyle {
-      Main: lg.NewStyle().
-         Foreground(lg.Color("#bac2de")),
-      Prefix: lg.NewStyle(),
-      Suffix: lg.NewStyle(),
-   }
-   unfocussedItemSelected := ItemStyle {
-      Main: unfocussedItemNormal.Main.
-         Bold(true).
-         Foreground(lg.Color("#f5e0dc")),
-      Prefix: unfocussedItemNormal.Prefix.
-         Foreground(lg.Color("#f9e2af")),
-      Suffix: unfocussedItemNormal.Suffix,
-   }
-   focussedItemNormal := ItemStyle {
-      Main: unfocussedItemNormal.Main.
-         Foreground(lg.Color("#cdd6f4")),
-      Prefix: unfocussedItemNormal.Prefix,
-      Suffix: unfocussedItemNormal.Suffix,
-   }
-   focussedItemSelected := ItemStyle {
-      Main: focussedItemNormal.Main.
-         Foreground(lg.Color("#f9e2af")),
-      Prefix: focussedItemNormal.Prefix.
-         Foreground(lg.Color("#fab387")),
-      Suffix: focussedItemNormal.Suffix,
-   }
-   unfocussed = Styles {
-      List: lg.NewStyle(),
-      Item: ItemStyleStates {
-         Normal: unfocussedItemNormal,
-         Selected: unfocussedItemSelected,
-      },
-   }
-   focussed = Styles {
-      List: lg.NewStyle(),
-      Item: ItemStyleStates {
-         Normal: focussedItemNormal,
-         Selected: focussedItemSelected,
-      },
-   }
-   return
-}
-
-// DefaultItemStyle returns a new ItemStyleStates struct with the given styles.
-
 
 
 // CachedItem is a struct that holds _unstyled_ rendered strings for the prefix, item, and suffix of an item,
